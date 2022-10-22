790f8182dee79bc0eb3fd98f8b74eedf687cce5e
Andres Letelier <almlogistico@gmail.com>
2022-10-22 15:42:38 -0300

    Actualiza propiedad hover menu y elipses

--- src/components/Textomenu.jsx
+++ src/components/Textomenu.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../styles/Textomenu.css'
+
 export const Textomenu = (props) => {
   return (
     <>
         <ul>
             <li>
-                <a href='g'>{ props.menu}</a>
+                <a href='a'>{ props.menu}</a>
             </li>
         </ul>
     </>
