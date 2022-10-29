6326ab1d0171b6560df3e797b9ad555ea6ea5cd0
Andres Letelier <almlogistico@gmail.com>
2022-10-29 14:50:55 -0300

    modifica App component

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ import Warning from "./components/Warning";
 function App() {
   return (
     <>
-        <Warning  title='Free Delivery over $100. Gift with Purchase over $150'/>
-        <Header />
-        <Tellafriend />           
+    
+      <div className='App'>
+          <Warning  title='Free Delivery over $100. Gift with Purchase over $150'/>
+          <Header />
+          <Tellafriend />  
+      </div>         
       
     </>
   );
