50417f3a2b7ce98f6a457f40042ec2e0b475edfb
Andres Letelier <almlogistico@gmail.com>
2022-10-28 22:44:48 -0300

    Refactorizo archivos como componetes dentro de carpetas

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 
 import "./App.css";
 import Header from "./components/Header";
-import Slider from "./components/Slider";
-import Tellafriend from "./components/Tellafriend";
-
-
+import Tellafriend from "./components/Tellafriends";
 import Warning from "./components/Warning";
+
 
 function App() {
   return (
     <>
-      <div className='App'>
-        <div className='warning'>
-            <Warning titulo='Free Delivery over $100. Gift with Purchase over $150'/>
-        </div>
-        <div className='contenedor-header'>
-            <Header />
-        </div>
-        <div>
-           <Tellafriend esHeader={ false } />
-        </div>
-        <div>
-          <Slider />
-        </div>
-      </div>         
+        <Warning  title='Free Delivery over $100. Gift with Purchase over $150'/>
+        <Header />
+        <Tellafriend />           
       
     </>
   );
