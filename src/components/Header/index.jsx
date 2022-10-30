997b20a790cf2329bf0193d8a5f52d2a8be0a95d
Andres Letelier <almlogistico@gmail.com>
2022-10-30 20:08:51 -0300

    avance responsive header-instagram-tellafriend

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -12,14 +12,14 @@ const Header = () => {
           className={style.imagen_logo}
           src={require('./images/logo.png')}
           alt='Logo Empresa' />
-      </div>
-      <div className={style.contenedor_menu}>
+      </div>      
+      <div className={style.contenedor_menu}>       
         <Textomenu menu='Ocassions'/>     
         <Textomenu menu='All Categories'/>
         <Textomenu menu='Gifts & Bundles'/>
         <Textomenu menu='Our Brands'/>
         <Textomenu menu='Abaout Us'/> 
-    </div>
+      </div>
       <div className={style.contenedor_elipse}>        
         <Elipse name='lupa' isHeader={ true } path='Header'/>
         <Elipse name='usuario' isHeader={ true } path='Header' />
