8b465140d013c1c2f6a51be4e4878113eacf3d78
Nrz24 <nico13544@gmail.com>
2022-11-17 09:48:04 -0300

    cambio de fuentes en componentes whomakes y ourpartner

--- src/components/WhoMakes/index.jsx
+++ src/components/WhoMakes/index.jsx
@@ -8,8 +8,8 @@ function WhoMakes() {
     return (
     
         <div className={styles.whoContainer}>
-        <h2 className={styles.whoTituloNegro}>Who Makes <span className={styles.whoTituloNaranja}>Your Food?</span> </h2>
-        <p className={styles.whoParrafo}>Our mission is to help grow the local food ecosystem in BC and across Canada.  Why? </p>
+            <h2 className={styles.whoTituloNegro}>Who Makes <span className={styles.whoTituloNaranja}>Your Food?</span> </h2>
+            <p className={styles.whoParrafo}>Our mission is to help grow the local food ecosystem in BC and across Canada.  Why? </p>
         <div className={styles.whoContainerimg}>
         </div>
             
