47fdde097fa9cfbb8625740cc5a342aa13593339
Francisco Pandolfi Jimenez <107133591+FranciscoPJ@users.noreply.github.com>
2022-11-21 19:12:26 -0300

    creacion de la API REST

--- src/components/SurpriseSomeone/SurpriseSomeone.jsx
+++ src/components/SurpriseSomeone/SurpriseSomeone.jsx
@@ -12,8 +12,11 @@ function SurpriseSomeone(){
             <div className={styles.text}>
                 <p className={styles.tituloNaranja}>Surprise Someone</p>
                 <p className={styles.tituloNegro}>Working from Home</p>
-                <p>Do you have a colleague or family member working hard at home? Our Working From Home box of snacks and drinks is the perfect way to put a smile on their face, delivered direct to their door.
-To buy for a large group of people, get in touch and we can discuss bespoke bundles and personalised messaging.</p>
+                <p>Do you have a colleague or family member working hard at home? 
+                <br/>
+                Our Working From Home box of snacks and drinks is the perfect way to<br/>put a smile on their face, delivered direct to their door.
+                <br/><br/>
+To buy for a large group of people, get in touch and we can discuss<br/>bespoke bundles and personalised messaging.</p>
                 <Button texto="Buy Now" />
             </div>
 
