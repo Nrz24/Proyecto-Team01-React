fcc7b6227fd6cf0c8b434be5096e0f85ee7555c6
diego-ismail <die.ibarra@gmail.com>
2022-11-24 16:34:44 -0300

    Cambios responsive ancho 1920

--- src/components/LatestOffers/index.jsx
+++ src/components/LatestOffers/index.jsx
@@ -1,29 +1,26 @@
-import styles from "./styles.module.css"
+import styles from "./styles.module.css";
 import CardProducto from "../CardProducto";
-import Carousel from "../Carousel/Carousel"
-
-
-
+import Carousel from "../Carousel/Carousel";
 
 function LatestOffers(props) {
-  const{cartItems, onAdd} = props;
+  const { onAdd } = props;
 
-    
-      return (
-        <>
-        <h2 className={styles.tituloNegro}>Latest <span className={styles.tituloNaranja}>Offers</span>  </h2>
-     
-          <Carousel  infiniteLoop={true} cardWidth="270" >     
-                <CardProducto item="pizza" onAdd={onAdd}/>  
-                <CardProducto item="pancakes" onAdd={onAdd}/>
-                <CardProducto item="orange" onAdd={onAdd}/>
-                <CardProducto item="avocado" onAdd={onAdd}/>
-                <CardProducto item="beer" onAdd={onAdd}/>
-                <CardProducto item="burger" onAdd={onAdd}/>    
-          </Carousel>
-      </>
+  return (
+    <>
+      <h2 className={styles.tituloNegro}>
+        Latest <span className={styles.tituloNaranja}>Offers</span>{" "}
+      </h2>
 
-      );
-    }
-    
-    export default LatestOffers;
+      <Carousel infiniteLoop={true} cardWidth="270">
+        <CardProducto item="pizza" onAdd={onAdd} />
+        <CardProducto item="pancakes" onAdd={onAdd} />
+        <CardProducto item="orange" onAdd={onAdd} />
+        <CardProducto item="avocado" onAdd={onAdd} />
+        <CardProducto item="beer" onAdd={onAdd} />
+        <CardProducto item="burger" onAdd={onAdd} />
+      </Carousel>
+    </>
+  );
+}
+
+export default LatestOffers;
